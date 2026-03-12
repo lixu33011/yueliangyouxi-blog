4f6621e50b6456f46f7756a1d95af098579a1b64
lixu33011 <442019072@qq.com>
2026-03-12 16:15:52 +0800

    Add configuration for routing and environment settings

---
+++ index.php
@@ -0,0 +1,97 @@
+{
+  "version": 2,
+  "routes": [
+    {
+      "src": "/",
+      "dest": "/api/index.js"
+    },
+    {
+      "src": "/index.php",
+      "dest": "/api/index.js"
+    },
+    {
+      "src": "/article-list.php",
+      "dest": "/api/index.js"
+    },
+    {
+      "src": "/article-detail.php",
+      "dest": "/api/index.js"
+    },
+    {
+      "src": "/admin",
+      "dest": "/api/index.js"
+    },
+    {
+      "src": "/admin/index.php",
+      "dest": "/api/index.js"
+    },
+    {
+      "src": "/backgrounds/bg1.css",
+      "dest": "/backgrounds/bg1.css"
+    },
+    {
+      "src": "/backgrounds/bg2.css",
+      "dest": "/backgrounds/bg2.css"
+    },
+    {
+      "src": "/backgrounds/bg3.css",
+      "dest": "/backgrounds/bg3.css"
+    },
+    {
+      "src": "/backgrounds/bg4.css",
+      "dest": "/backgrounds/bg4.css"
+    },
+    {
+      "src": "/templates/home/home1.css",
+      "dest": "/templates/home/home1.css"
+    },
+    {
+      "src": "/templates/home/home2.css",
+      "dest": "/templates/home/home2.css"
+    },
+    {
+      "src": "/templates/article-list/list1.css",
+      "dest": "/templates/article-list/list1.css"
+    },
+    {
+      "src": "/templates/article-list/list2.css",
+      "dest": "/templates/article-list/list2.css"
+    },
+    {
+      "src": "/templates/article-detail/detail1.css",
+      "dest": "/templates/article-detail/detail1.css"
+    },
+    {
+      "src": "/templates/article-detail/detail2.css",
+      "dest": "/templates/article-detail/detail2.css"
+    },
+    {
+      "src": "/data/config.json",
+      "dest": "/data/config.json"
+    },
+    {
+      "src": "/data/articles.json",
+      "dest": "/data/articles.json"
+    },
+    {
+      "src": "/data/links.json",
+      "dest": "/data/links.json"
+    },
+    {
+      "src": "/data/backgrounds.json",
+      "dest": "/data/backgrounds.json"
+    },
+    {
+      "src": "/data/templates.json",
+      "dest": "/data/templates.json"
+    },
+    {
+      "src": "/uploads/(.*)",
+      "dest": "/uploads/$1"
+    }
+  ],
+  "env": {
+    "PHP_VERSION": "7.4",
+    "NODE_ENV": "production"
+  }
+}
